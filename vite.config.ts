a523fb8d5635e44a005d0bdf538c1ccc16335bf4
saweratariq352-ctrl <saweratariq352@gmail.com>
2026-01-07 12:13:35 +0500

    Fix vite config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 
   resolve: {
     alias: {
@@ -15,23 +12,13 @@ export default defineConfig({
     },
   },
 
+  define: {
+    "process.env": process.env,
+  },
+
   build: {
     outDir: "dist",
     sourcemap: false,
-
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) {
-              return "react";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
-
     chunkSizeWarningLimit: 800,
   },
 });
